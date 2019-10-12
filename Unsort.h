76b9c1a6b1c73a2a48f09a11e79c94531313f8a2
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2019-10-12 12:28:00 +0200

    Update unsorter to handle multiple zipped vectors.

--- Unsort.h
+++ Unsort.h
@@ -35,21 +35,65 @@ private:
     std::vector<std::size_t> m_originalIndices;
 };
 
+namespace detail
+{
+    // https://stackoverflow.com/a/17074810/3763139
+
+    template <typename T, typename Compare>
+    static std::vector<std::size_t> sort_permutation(
+        const std::vector<T>& vec,
+        Compare& compare)
+    {
+        std::vector<std::size_t> p(vec.size());
+        std::iota(p.begin(), p.end(), 0);
+        std::sort(p.begin(), p.end(),
+            [&](std::size_t i, std::size_t j) { return compare(vec[i], vec[j]); });
+        return p;
+    }
+
+    template <typename T>
+    static void apply_permutation_in_place(
+        std::vector<T>& vec,
+        const std::vector<std::size_t>& p)
+    {
+        std::vector<std::uint8_t> done(vec.size());
+        for (std::size_t i = 0; i < vec.size(); ++i)
+        {
+            if (done[i])
+            {
+                continue;
+            }
+            done[i] = true;
+            std::size_t prev_j = i;
+            std::size_t j = p[i];
+            while (i != j)
+            {
+                std::swap(vec[prev_j], vec[j]);
+                done[j] = true;
+                prev_j = j;
+                j = p[j];
+            }
+        }
+    }
+}
+
 template <typename T, typename CompareT = std::less<>>
 [[nodiscard]] Unsorter reversibleSort(std::vector<T> & values, CompareT cmp = CompareT{})
 {
-    const std::size_t size = values.size();
-    std::vector<std::size_t> originalIndices(size);
-    std::iota(originalIndices.begin(), originalIndices.end(), 0);
+    auto perm = detail::sort_permutation(values, cmp);
 
-    std::sort(
-        originalIndices.begin(),
-        originalIndices.end(),
-        [&values, cmp](std::size_t lhs, std::size_t rhs) {
-            return cmp(values[lhs], values[rhs]);
-        }
-    );
-    std::sort(values.begin(), values.end(), cmp);
+    detail::apply_permutation_in_place(values, perm);
 
-    return Unsorter(std::move(originalIndices));
+    return Unsorter(std::move(perm));
+}
+
+template <typename T, typename U, typename CompareT = std::less<>>
+[[nodiscard]] Unsorter reversibleZipSort(std::vector<T>& keys, std::vector<U>& values, CompareT cmp = CompareT{})
+{
+    auto perm = detail::sort_permutation(keys, cmp);
+
+    detail::apply_permutation_in_place(keys, perm);
+    detail::apply_permutation_in_place(values, perm);
+
+    return Unsorter(std::move(perm));
 }
