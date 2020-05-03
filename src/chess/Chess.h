67431ed81a461abf353f7cd5a4a238a1dc5cc91f
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-05-03 18:48:37 +0200

    Fix square color computation.

--- src/chess/Chess.h
+++ src/chess/Chess.h
@@ -678,7 +678,7 @@ public:
     [[nodiscard]] constexpr Color color() const
     {
         ASSERT(isOk());
-        return fromOrdinal<Color>((~static_cast<unsigned>(m_id)) & 1);
+        return !fromOrdinal<Color>(ordinal(rank()) + ordinal(file()) & 1);
     }
 
     constexpr void flipVertically()
@@ -772,6 +772,9 @@ constexpr Square h6(fileH, rank6);
 constexpr Square h7(fileH, rank7);
 constexpr Square h8(fileH, rank8);
 
+static_assert(e1.color() == Color::Black);
+static_assert(e8.color() == Color::White);
+
 template <>
 struct EnumTraits<Square>
 {
@@ -782,14 +785,14 @@ struct EnumTraits<Square>
     static constexpr bool isNaturalIndex = true;
 
     static constexpr std::array<EnumType, cardinality> values{
-        a1, a2, a3, a4, a5, a6, a7, a8,
-        b1, b2, b3, b4, b5, b6, b7, b8,
-        c1, c2, c3, c4, c5, c6, c7, c8,
-        d1, d2, d3, d4, d5, d6, d7, d8,
-        e1, e2, e3, e4, e5, e6, e7, e8,
-        f1, f2, f3, f4, f5, f6, f7, f8,
-        g1, g2, g3, g4, g5, g6, g7, g8,
-        h1, h2, h3, h4, h5, h6, h7, h8
+        a1, b1, c1, d1, e1, f1, g1, h1,
+        a2, b2, c2, d2, e2, f2, g2, h2,
+        a3, b3, c3, d3, e3, f3, g3, h3,
+        a4, b4, c4, d4, e4, f4, g4, h4,
+        a5, b5, c5, d5, e5, f5, g5, h5,
+        a6, b6, c6, d6, e6, f6, g6, h6,
+        a7, b7, c7, d7, e7, f7, g7, h7,
+        a8, b8, c8, d8, e8, f8, g8, h8
     };
 
     [[nodiscard]] static constexpr int ordinal(EnumType c) noexcept
@@ -837,6 +840,7 @@ struct EnumTraits<Square>
 };
 
 static_assert(toString(d1) == std::string_view("d1"));
+static_assert(values<Square>()[29] == f4);
 
 enum struct MoveType : std::uint8_t
 {
