9b26995e68379a84eeb385c73884cd93d0a9c998
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-05-17 16:16:29 +0200

    remove some leftover

--- src/CommandLineTool.cpp
+++ src/CommandLineTool.cpp
@@ -1714,10 +1714,6 @@ namespace command_line_app
                 case 1:
                     header.compressionLevel = bcgn::BcgnCompressionLevel::Level_1;
                     break;
-
-                case 2:
-                    header.compressionLevel = bcgn::BcgnCompressionLevel::Level_2;
-                    break;
                 }
             }
 
