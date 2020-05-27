622d9abb37bfaead43eb9e2eb06dc1f751cb05af
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-05-27 13:24:21 +0200

    change the semantics of bit packing to be more in line with bit fields

--- src/chess_pos_db.cpp
+++ src/chess_pos_db.cpp
@@ -39,8 +39,6 @@ void runCommand(const std::vector<std::string>& args)
     command_line_app::runCommand(args);
 }
 
-#include "util/BitPacking.h"
-
 int main(int argc, char* argv[])
 {
     if (argc == 1)
--- src/util/BitPacking.h
+++ src/util/BitPacking.h
@@ -26,74 +26,74 @@ namespace util
     // For example to make BitSpans with the same mask
     // but being contained by different PackedInts
     // not being interchangable.
-    template <typename IntT, IntT MaskV, std::size_t IdV = 0>
-    struct BitSpan
+    template <typename T, typename IntT, IntT MaskV, std::size_t IdV = 0>
+    struct BitField
     {
         static_assert(std::is_unsigned_v<IntT>, "Bit span may only operate on unsigned types.");
         static_assert(MaskV, "Bit span must consist of at least one bit.");
         static_assert(detail::hasContiguousSetBits(MaskV), "All set bits in the bit span must be contiguous.");
+        
+        static_assert(std::is_integral_v<T>);
 
-        using UnsignedInt = IntT;
-        using SignedInt = std::make_signed_t<IntT>;
+        using UnderlyingType = IntT;
+        using ValueType = T;
 
         static constexpr IntT mask = MaskV;
         static constexpr IntT shift = intrin::lsb_constexpr(MaskV);
         static constexpr IntT size = intrin::popcount_constexpr(MaskV);
 
-        template <typename T>
+        static_assert(sizeof(T) * 8 >= size, "The result type must be large enough to contain the loaded value.");
+
+        static constexpr bool isValueTypeUnsigned = std::is_unsigned_v<T>;
+
         [[nodiscard]] static constexpr IntT storeSafe(T v)
         {
             // precondition: msb(v) < size
-            if constexpr (std::is_signed_v<T>)
-            {
-                using Unsigned = std::make_unsigned_t<T>;
-                return storeUnsigned(static_cast<Unsigned>(v)) & mask;
-            }
-            else
+            if constexpr (isValueTypeUnsigned)
             {
                 return storeUnsigned(v) & mask;
             }
+            else
+            {
+                return storeUnsigned(static_cast<IntT>(v)) & mask;
+            }
         }
 
-        template <typename T>
         [[nodiscard]] static constexpr IntT store(T v)
         {
             // precondition: msb(v) < size
-            if constexpr (std::is_signed_v<T>)
-            {
-                using Unsigned = std::make_unsigned_t<T>;
-                // we & with mask to ensure negative values don't spill
-                return storeUnsigned(static_cast<Unsigned>(v)) & mask;
-            }
-            else
+            if constexpr (isValueTypeUnsigned)
             {
                 return storeUnsigned(v);
             }
+            else
+            {
+                // we & with mask to ensure negative values don't spill
+                return storeUnsigned(static_cast<IntT>(v)) & mask;
+            }
         }
 
-        template <typename T>
         [[nodiscard]] static constexpr T load(IntT v)
         {
-            static_assert(sizeof(T) * 8 >= size, "The result type must be large enough to contain the loaded value.");
-
-            if constexpr (std::is_signed_v<T>)
+            if constexpr (isValueTypeUnsigned)
             {
-                using Unsigned = std::make_unsigned_t<T>;
-                const auto uns = loadUnsigned<Unsigned>(v);
-                return signExtend<size, Unsigned, T>(uns);
+                return loadUnsigned(v);
             }
             else
             {
-                return loadUnsigned<T>(v);
+                const auto uns = loadUnsigned(v);
+                return signExtend<size, IntT, T>(uns);
             }
         }
 
-    private:
-        template <typename T>
-        [[nodiscard]] static constexpr IntT storeUnsigned(T v)
+        [[nodiscard]] static constexpr IntT loadRaw(IntT v)
         {
-            static_assert(std::is_unsigned_v<T>);
+            return v & mask;
+        }
 
+    private:
+        [[nodiscard]] static constexpr IntT storeUnsigned(IntT v)
+        {
             // The compiler probably could do it by itself but may as well explicitely
             if constexpr (shift == 0)
             {
@@ -105,12 +105,8 @@ namespace util
             }
         }
 
-        template <typename T>
-        [[nodiscard]] static constexpr T loadUnsigned(IntT v)
+        [[nodiscard]] static constexpr IntT loadUnsigned(IntT v)
         {
-            static_assert(std::is_unsigned_v<T>);
-            static_assert(sizeof(T) * 8 >= size);
-
             // The compiler probably could do it by itself but may as well explicitely
             if constexpr (shift == 0)
             {
@@ -145,15 +141,14 @@ namespace util
             == (intrin::popcount_constexpr(BitSpanTs::mask) + ...),
             "Bit spans have overlapping masks.");
 
-        static_assert(meta::AreAllTheSame<typename BitSpanTs::UnsignedInt...>::value);
+        static_assert(meta::AreAllTheSame<typename BitSpanTs::UnderlyingType...>::value);
 
         using FirstBitSpanT = typename meta::FirstInPack<BitSpanTs...>::type;
 
-        using UnsignedInt = typename FirstBitSpanT::UnsignedInt;
-        using SignedInt = typename FirstBitSpanT::SignedInt;
+        using UnderlyingType = typename FirstBitSpanT::UnderlyingType;
 
-        static constexpr UnsignedInt mask = (BitSpanTs::mask | ...);
-        static constexpr UnsignedInt size = (BitSpanTs::size + ...);
+        static constexpr UnderlyingType mask = (BitSpanTs::mask | ...);
+        static constexpr UnderlyingType size = (BitSpanTs::size + ...);
 
         constexpr PackedInts() :
             m_value(0)
@@ -181,43 +176,50 @@ namespace util
         constexpr PackedInts& operator=(const PackedInts&) = default;
         constexpr PackedInts& operator=(PackedInts&&) = default;
 
-        template <typename BitSpanT, typename T>
-        [[nodiscard]] constexpr T get()
+        template <typename BitSpanT>
+        [[nodiscard]] constexpr auto get()
         {
             static_assert(meta::IsContained<BitSpanT, BitSpanTs...>::value, "The bit span doesn't belong to this type.");
-            return BitSpanT::template load<T>(m_value);
+            return BitSpanT::template load(m_value);
         }
 
-        template <typename BitSpanT, typename T>
-        constexpr void init(T v)
+        template <typename... LocalBitSpanTs>
+        [[nodiscard]] constexpr auto getRaw()
+        {
+            static_assert((meta::IsContained<LocalBitSpanTs, BitSpanTs...>::value && ...), "The bit span doesn't belong to this type.");
+            return m_value & (LocalBitSpanTs::mask | ...);
+        }
+
+        template <typename BitSpanT>
+        constexpr void init(typename BitSpanT::ValueType v)
         {
             // precondition: value of this type was not yet set
             static_assert(meta::IsContained<BitSpanT, BitSpanTs...>::value, "The bit span doesn't belong to this type.");
             m_value |= BitSpanT::store(v);
         }
 
-        template <typename BitSpanT, typename T>
-        constexpr void set(T v)
+        template <typename BitSpanT>
+        constexpr void set(typename BitSpanT::ValueType v)
         {
             static_assert(meta::IsContained<BitSpanT, BitSpanTs...>::value, "The bit span doesn't belong to this type.");
             m_value &= ~BitSpanT::mask;
             m_value |= BitSpanT::store(v);
         }
 
-        template <typename BitSpanT, typename T>
-        constexpr void setSafe(T v)
+        template <typename BitSpanT>
+        constexpr void setSafe(typename BitSpanT::ValueType v)
         {
             static_assert(meta::IsContained<BitSpanT, BitSpanTs...>::value, "The bit span doesn't belong to this type.");
             m_value &= ~BitSpanT::mask;
             m_value |= BitSpanT::storeSafe(v);
         }
 
-        [[nodiscard]] constexpr UnsignedInt value() const
+        [[nodiscard]] constexpr UnderlyingType value() const
         {
             return m_value;
         }
 
     private:
-        UnsignedInt m_value;
+        UnderlyingType m_value;
     };
 }
